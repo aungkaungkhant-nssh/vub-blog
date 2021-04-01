14a0334f75160f0302097cce072e0ef680afa156
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-04-01 20:02:51 +0630

    create post and style frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Create from '../views/Create.vue'
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path:"/create",
+    name:"Create",
+    component:Create
   }
   
 ]
